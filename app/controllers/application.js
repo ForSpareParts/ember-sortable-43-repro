d336efd7436cd773493ef8665747fd0794a553f7
David Jackson <david.jackson@sykes.com>
2015-07-30 10:14:43 -0500

    Repro for ember-sortable issue #43: https://github.com/jgwhite/ember-sortable/issues/43

---
+++ app/controllers/application.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  items: [
+    { name: 'Uno' },
+    { name: 'Dos' },
+    { name: 'Tres' },
+    { name: 'Cuatro' },
+    { name: 'Cinco' }
+  ]
+});
